dd14654b0a3ffb7e841e7bfb352a2e82e751fc3e
Jonesloto <70425494+Jonesloto@users.noreply.github.com>
2020-11-01 21:23:08 -0500

    moved "create_module" method into framework_builde

--- Plugin/plugin_runner/framework_builder/init.lua
+++ Plugin/plugin_runner/framework_builder/init.lua
@@ -12,6 +12,86 @@ function FrameworkBuilder:get_load_keyword()
     return LOADED
 end
 
+function FrameworkBuilder:sample_module_index()
+    return {
+        ["service"] = {
+            sample = script.Parent.Parent.module_components.server_assets.service;
+            location = ServerScriptService:WaitForChild("GameServer"):WaitForChild("core_modules"):WaitForChild("services");
+            sample_name = "MyService";
+        };
+    
+        ["server_class"] = {
+            sample = script.Parent.Parent.module_components.server_assets.server_class;
+            location = ServerScriptService:WaitForChild("GameServer"):WaitForChild("core_modules"):WaitForChild("classes");
+            sample_name = "MyClass";
+        };
+        
+        ["server_module"] = {
+            sample = script.Parent.Parent.module_components.server_assets.server_module;
+            location = ServerScriptService:WaitForChild("GameServer"):WaitForChild("core_modules"):WaitForChild("modules");
+            sample_name = "MyModule";
+        };
+    
+        ["controller"] = {
+            sample = script.Parent.Parent.module_components.client_assets.controller;
+            location = StarterPlayer.StarterPlayerScripts:WaitForChild("GameClient"):WaitForChild("core_modules"):WaitForChild("controllers");
+            sample_name = "MyController";
+        };
+    
+        ["client_class"] = {
+            sample = script.Parent.Parent.module_components.client_assets.client_class;
+            location = StarterPlayer.StarterPlayerScripts:WaitForChild("GameClient"):WaitForChild("core_modules"):WaitForChild("classes");
+            sample_name = "MyClass";
+        };
+    
+        ["client_module"] = {
+            sample = script.Parent.Parent.module_components.client_assets.client_module;
+            location = StarterPlayer.StarterPlayerScripts:WaitForChild("GameClient"):WaitForChild("core_modules"):WaitForChild("modules");
+            sample_name = "MyModule";
+        };
+    
+        ["shared_class"] = {
+            sample = script.Parent.Parent.module_components.shared_assets.shared_class;
+            location = ReplicatedStorage:WaitForChild("GameShared"):WaitForChild("core_modules"):WaitForChild("modules");
+            sample_name = "MyClass";
+        };
+    
+        ["shared_module"] = {
+            sample = script.Parent.Parent.module_components.shared_assets.shared_module;
+            location = ReplicatedStorage:WaitForChild("GameShared"):WaitForChild("core_modules"):WaitForChild("modules");
+            sample_name = "MyModule";
+        };
+    }
+end
+
+function FrameworkBuilder:build_module(options)
+    local sample_module_index = self:sample_module_index()
+
+    local loaded_server = ServerScriptService:FindFirstChild(LOADED)
+    local loaded_client = StarterPlayer.StarterPlayerScripts:FindFirstChild(LOADED)
+    local loaded_shared = ReplicatedStorage:FindFirstChild(LOADED)
+
+    if not (loaded_server and loaded_client and loaded_shared) then return warn("Did not detect Jonii Express environment. Failed to create module.") end
+
+    local module_type = options.module_type
+    local module_name = options.module_name
+    local player_name = options.player_name
+
+    if not sample_module_index[module_type] then return warn("invalid source module type!") end
+
+    local module = sample_module_index[module_type].sample:Clone()
+
+    module.Name = module_name
+
+    module.Source = module.Source:gsub("%-- Created by user", "-- Created by "..player_name):gsub("%-- Date: 99/99/9999", "-- Date: "..os.date("%x", tick()))
+
+    if module_name then
+        module.Source = module.Source:gsub(sample_module_index[module_type].sample_name, module_name)
+    end
+
+    module.Parent = sample_module_index[module_type].location
+end
+
 function FrameworkBuilder:convert_table_to_object(options)
     options = options or {}
 
